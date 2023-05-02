e3238972ac992f534da99473c926adc0a312550f
James <>
2023-05-01 21:54:12 -0500

    restart button properly clears board

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const restart = document.getElementById('restart')
 
+restart.addEventListener('click', function(){
+    document.getElementById('cell1').innerText = ''
+    document.getElementById('cell2').innerText = ''
+    document.getElementById('cell3').innerText = ''
+    document.getElementById('cell4').innerText = ''
+    document.getElementById('cell5').innerText = ''
+    document.getElementById('cell6').innerText = ''
+    document.getElementById('cell7').innerText = ''
+    document.getElementById('cell8').innerText = ''
+    document.getElementById('cell9').innerText = ''
 
-    
+})
