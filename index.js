4cd2a26c9889d4fdcb545eff6778f7ef3117bfb7
James <>
2023-05-01 21:59:46 -0500

    renamed cell variables

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const restart = document.getElementById('restart')
 
-restart.addEventListener('click', function(){
-    document.getElementById('cell1').innerText = ''
-    document.getElementById('cell2').innerText = ''
-    document.getElementById('cell3').innerText = ''
-    document.getElementById('cell4').innerText = ''
-    document.getElementById('cell5').innerText = ''
-    document.getElementById('cell6').innerText = ''
-    document.getElementById('cell7').innerText = ''
-    document.getElementById('cell8').innerText = ''
-    document.getElementById('cell9').innerText = ''
+const cell1 = document.getElementById('cell1')
+const cell2 = document.getElementById('cell2')
+const cell3 = document.getElementById('cell3')
+const cell4 = document.getElementById('cell4')
+const cell5 = document.getElementById('cell5')
+const cell6 = document.getElementById('cell6')
+const cell7 = document.getElementById('cell7')
+const cell8 = document.getElementById('cell8')
+const cell9 = document.getElementById('cell9')
 
+restart.addEventListener('click', function(){
+    cell1.innerText = ''
+    cell2.innerText = ''
+    cell3.innerText = ''
+    cell4.innerText = ''
+    cell5.innerText = ''
+    cell6.innerText = ''
+    cell7.innerText = ''
+    cell8.innerText = ''
+    cell9.innerText = ''
 })
